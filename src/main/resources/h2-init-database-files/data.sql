7fb068f2500a4a867169fb2ad12d3bd8f4281fa3
Jakub Maciocha <jakub.maciocha@gmail.com>
2025-05-28 19:56:27 +0200

    Resources changes and adding test data for previous changes

--- src/main/resources/h2-init-database-files/data.sql
+++ src/main/resources/h2-init-database-files/data.sql
@@ -1,24 +1,43 @@
-INSERT INTO sensors
+INSERT INTO gg_users
+VALUES (
+           'admin',
+           '$2a$10$agqKO9An2MIiTejIK3cpO.tYp3lAw30s7uRiDGBSGmpISmeHo865G'
+       );
+
+INSERT INTO sensors(id, username, name, ip_address, mac_address,  is_active )
 VALUES (
         RANDOM_UUID(),
+        'admin',
         'sensor 1',
         '192.168.1.2',
         '00:1A:2B:3C:4D:5E',
         true
        );
 
-INSERT INTO sensors
+INSERT INTO sensors(id, username, name, ip_address, mac_address, is_active )
 VALUES (
         RANDOM_UUID(),
+        'admin',
         'sensor 2',
         '192.168.1.3',
         'A1:B2:C3:D4:E5:F6',
         true
     );
 
-INSERT INTO sensors(id, ip_address, mac_address)
+INSERT INTO sensors(id, username, name, ip_address, mac_address,  is_active )
+VALUES (
+           RANDOM_UUID(),
+           'admin',
+           'sensor 5',
+           '192.168.1.5',
+           'A1:B2:C3:D4:E5:10',
+           false
+       );
+
+INSERT INTO sensors(id, username, ip_address, mac_address)
 VALUES (
        RANDOM_UUID(),
+       'admin',
        '192.168.1.4',
        '12:34:56:78:9A:BC'
        );
@@ -32,9 +51,48 @@ VALUES (
         CURRENT_TIMESTAMP()
        );
 
-
-INSERT INTO gg_users
+INSERT INTO readings
 VALUES (
-        'admin',
-        '$2a$10$YLMG97UK53w.fAkh2Ys/uel3yvNtTg0SfpAbpoan0nBIgVmHtv8y6'
+           RANDOM_UUID(),
+           SELECT id FROM sensors WHERE sensors.name='sensor 1',
+           10.25,
+           44.10,
+           CURRENT_TIMESTAMP()
        );
+
+INSERT INTO readings
+VALUES (
+           RANDOM_UUID(),
+           SELECT id FROM sensors WHERE sensors.name='sensor 1',
+           12.25,
+           47.10,
+           CURRENT_TIMESTAMP()
+       );
+
+INSERT INTO readings
+VALUES (
+           RANDOM_UUID(),
+           SELECT id FROM sensors WHERE sensors.name='sensor 2',
+           25.23,
+           20.10,
+           CURRENT_TIMESTAMP()
+       );
+
+INSERT INTO readings
+VALUES (
+           RANDOM_UUID(),
+           SELECT id FROM sensors WHERE sensors.name='sensor 2',
+           22.10,
+           34.10,
+           CURRENT_TIMESTAMP()
+       );
+
+INSERT INTO readings
+VALUES (
+           RANDOM_UUID(),
+           SELECT id FROM sensors WHERE sensors.name='sensor 2',
+           20.25,
+           37.10,
+           CURRENT_TIMESTAMP()
+       );
+
--- src/main/resources/h2-init-database-files/schema.sql
+++ src/main/resources/h2-init-database-files/schema.sql
@@ -1,24 +1,29 @@
+DROP TABLE IF EXISTS gg_users;
+CREATE TABLE gg_users(
+    username VARCHAR(100) PRIMARY KEY,
+    password VARCHAR(255)
+);
+
 DROP TABLE IF EXISTS sensors;
 CREATE TABLE sensors(
     id UUID PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
     name VARCHAR(100) DEFAULT 'not specified',
     ip_address VARCHAR(17) NOT NULL,
     mac_address VARCHAR(17),
-    is_active BOOL DEFAULT false
+    is_active BOOL DEFAULT false,
+
+    FOREIGN KEY (username) REFERENCES gg_users(username) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS readings;
 CREATE TABLE readings(
     id UUID PRIMARY KEY,
     sensor_id UUID NOT NULL,
-    temperature DECIMAL(5,2),
-    humidity DECIMAL(5,2),
-    capture_time TIMESTAMP DEFAULT NOW(),
+    temperature FLOAT,
+    humidity FLOAT,
+    capture_time TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+
     FOREIGN KEY (sensor_id) REFERENCES sensors(id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS gg_users;
-CREATE TABLE gg_users(
-    username VARCHAR(100) PRIMARY KEY,
-    password VARCHAR(255)
-)
